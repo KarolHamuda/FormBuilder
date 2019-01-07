c7abb0ccd8f3c94a07528db7732f93c022188f5b
Nemerianos <karol.hamuda@gmail.com>
2019-01-07 15:53:49 +0100

    Initial commit

---
+++ src/Components/NumberForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { } from 'react-bootstrap';
+
+import './App.css';
+
+class NumberForm extends Component {
+    render() {
+        return (
+          <div className="NumberForm">
+    
+          </div>
+        );
+      }
+    }
+    
+export default NumberForm;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Media, Row, Col } from 'react-bootstrap';
+
+import './App.css';
+
+class TextForm extends Component {
+    render() {
+        return (
+          <Media className="TextForm">
+            <Media.Left>
+ 
+            </Media.Left>
+
+            
+          </Media>
+        );
+      }
+    }
+    
+export default TextForm;
